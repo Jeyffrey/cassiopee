add6f64c0aac8d3726892158e3bc4ea10a227678
Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
2026-07-22 00:36:25 +0200

    tech: modifications for prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jeyffrey.github.io',
-  base: '/cassiopee',
+  site: 'https://choeurcassiopee.fr',
 });
